791dcfed963b3691de5b2d7de27b24f568ed11ac
YoYing <y.latzka@htlkrems.at>
2023-12-15 11:12:13 +0100

    Error handling registration

--- Backend/Graphql/Mutations/Mutation.cs
+++ Backend/Graphql/Mutations/Mutation.cs
@@ -1,3 +1,4 @@
+using Backend.Identity;
 using Domain.Repositories.Implementations;
 using Domain.Repositories.Interfaces;
 using HotChocolate.Authorization;
@@ -5,48 +6,45 @@ using Model.Entities;
 
 namespace Backend.Graphql.Mutations;
 
-public class Mutation
-{
+public class Mutation {
     [Authorize]
-    public async Task<User> AddFollow(int master_id, int slave_id, [Service] IUserFollowsRepository ufuRepo, [Service] IUserRepository userRepo)
-    {
+    public async Task<User> AddFollow(int master_id, int slave_id, [Service] IUserFollowsRepository ufuRepo,
+        [Service] IUserRepository userRepo) {
         var master = await userRepo.ReadAsync(slave_id);
-        
-        if (ufuRepo.IsFollowing(master_id, slave_id))
-        {
+
+        if (ufuRepo.IsFollowing(master_id, slave_id)) {
             return master;
-        } 
-        
-        var ufu = new UserFollowsUser()
-        {
+        }
+
+        var ufu = new UserFollowsUser() {
             MasterId = master_id,
             SlaveId = slave_id
         };
-        
+
         await ufuRepo.CreateAsync(ufu);
-        
+
         return master;
     }
-    
-    public async Task<User> UserRegistration(string username, string firstname, string lastname, string password, [Service] IUserRepository userRepo)
-    {
 
-        if (!userRepo.IsUsernameAvailable(username).Result) {
-            throw new Exception("Username is not available");
-        }
+    [Error(typeof(UsernameAlreadyTakenException))]
+    [Error(typeof(PasswordTooShortException))]
+    public async Task<User> UserRegistration(string username, string firstname, string lastname, string password,
+        [Service] IUserRepository userRepo, [Service] IUserService userService) {
+        if (!userRepo.IsUsernameAvailable(username).Result)
+            throw new UsernameAlreadyTakenException(username);
 
-        ;
+        if (password.Length < 8) 
+            throw new PasswordTooShortException();
 
-        var user = new User()
-        {
+        var user = new User {
             Username = username,
-            PasswordHash = password,
+            PasswordHash = userService.HashPassword(password),
             FirstName = firstname,
             LastName = lastname
         };
-        
+
         await userRepo.CreateAsync(user);
-        
+
         return user;
     }
 }
--- Backend/Identity/UserService.cs
+++ Backend/Identity/UserService.cs
@@ -4,33 +4,35 @@ using Model.Entities;
 
 namespace Backend.Identity;
 
-public interface IUserService
-{
+public interface IUserService {
     Task<User?> GetUser(string? username);
     bool IsAuthenticated(string password, User user);
+    string HashPassword(string password);
 }
 
-public sealed class UserService : IUserService
-{
+public sealed class UserService : IUserService {
     private readonly YDbContext _db;
 
-    public UserService(YDbContext db)
-    {
+    public UserService(YDbContext db) {
         _db = db;
     }
 
-    public async Task<User?> GetUser(string? username)
-    {
+    public async Task<User?> GetUser(string? username) {
         ArgumentNullException.ThrowIfNull(username);
 
         return await _db.Users.SingleOrDefaultAsync(u => u.Username == username);
     }
 
-    public bool IsAuthenticated(string password, User u)
-    {
+    public bool IsAuthenticated(string password, User u) {
         ArgumentNullException.ThrowIfNull(password);
         ArgumentNullException.ThrowIfNull(u);
 
         return BCrypt.Net.BCrypt.Verify(password, u.PasswordHash);
+    }
+
+    public string HashPassword(string password) {
+        ArgumentNullException.ThrowIfNull(password);
+
+        return BCrypt.Net.BCrypt.HashPassword(password);
     }
 }
