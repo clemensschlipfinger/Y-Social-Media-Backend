4945731fc18703b92c0a51b5a86b25ceb0030ad6
YoYing <y.latzka@htlkrems.at>
2023-12-15 11:12:26 +0100

    exeptions

---
+++ Backend/Graphql/Exeptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Graphql; 
+
+public class UsernameAlreadyTakenException : Exception
+{
+    public UsernameAlreadyTakenException(string username) : base($"Username {username} is already taken")
+    {
+    }
+}
+
+public class PasswordTooShortException : Exception
+{
+    public PasswordTooShortException() : base("Password is too short")
+    {
+    }
+}
