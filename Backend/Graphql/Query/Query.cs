b4ea0f7ba422d675070920bced9411df745dc354
YoYing <y.latzka@htlkrems.at>
2024-02-16 09:58:37 +0100

    ReadFullUser, ReadForYouPage fixed

--- Backend/Graphql/Query/Query.cs
+++ Backend/Graphql/Query/Query.cs
@@ -7,8 +7,7 @@ using Model.Entities;
 
 namespace Backend.Graphql.Query;
 
-public class Query
-{
+public class Query {
     public IQueryable<Yeet> GetYeets([Service] IYeetRepository repo)
         => repo.ReadFullYeet();
 
@@ -22,37 +21,8 @@ public class Query
     public IQueryable<User> GetUser(int userId, [Service] IUserRepository repo)
         => repo.Read(userId);
 
-    public IQueryable<FullUserDto> GetUsers([Service] IUserRepository userRepo,
-        [Service] IUserFollowsRepository userFollowsRepo)
-    {
-        var users = userRepo.ReadAll().ToList();
-        Console.WriteLine(String.Join(", ", users.Select(e => e.Username)));
-        
-        List<ExpandedUser> expUser = new();
-
-        foreach (var user in users)
-        {
-            var followerCount = userFollowsRepo.GetFollowersCount(user.Id).Count;
-            var followingCount = userFollowsRepo.GetFollowingCount(user.Id).Count;
-            var followers = userFollowsRepo.GetFollowers(user.Id)?.Select(u => u.Adapt<DefaultUserDto>()).ToList();
-            var followings = userFollowsRepo.GetFollowing(user.Id)?.Select(u => u.Adapt<DefaultUserDto>()).ToList();
-
-            expUser.Add(new ExpandedUser
-            {
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Username = user.Username,
-                Id = user.Id,
-                FollowerCount = followerCount,
-                FollowingCount = followingCount,
-                Followers = followers,
-                Followings = followings
-            });
-            Console.WriteLine(String.Join(", ", expUser.Select(e => e.Username)));
-        }
-
-        return expUser.Select(eu => eu.Adapt<FullUserDto>()).AsQueryable();
-    }
+    public IQueryable<FullUserDto> GetUsers([Service] IUserRepository userRepo)
+    => userRepo.ReadFullUsers();
 
     public CountUserDto GetFollowingCount(int userId, [Service] IUserFollowsRepository repo)
         => repo.GetFollowingCount(userId);
