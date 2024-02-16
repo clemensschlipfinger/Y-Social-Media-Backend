f0aae4b074374248cb661dd4c75346c37d058c7f
trueberryless <trueberryless+github@gmail.com>
2024-02-16 13:20:01 +0100

    fix migrations

---
+++ Model/Migrations/20240216121657_InitialCommit.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tag",
+                columns: table => new
+                {
+                    tag_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tag", x => x.tag_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    username = table.Column<string>(type: "text", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_follows_users_jt",
+                columns: table => new
+                {
+                    follower_id = table.Column<int>(type: "integer", nullable: false),
+                    following_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_follows_users_jt", x => new { x.follower_id, x.following_id });
+                    table.ForeignKey(
+                        name: "FK_user_follows_users_jt_users_follower_id",
+                        column: x => x.follower_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_follows_users_jt_users_following_id",
+                        column: x => x.following_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "yeets",
+                columns: table => new
+                {
+                    yeet_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    body = table.Column<string>(type: "text", nullable: false),
+                    likes = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yeets", x => x.yeet_id);
+                    table.ForeignKey(
+                        name: "FK_yeets_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "yeet_has_tags",
+                columns: table => new
+                {
+                    yeet_id = table.Column<int>(type: "integer", nullable: false),
+                    tag_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yeet_has_tags", x => new { x.yeet_id, x.tag_id });
+                    table.ForeignKey(
+                        name: "FK_yeet_has_tags_tag_tag_id",
+                        column: x => x.tag_id,
+                        principalTable: "tag",
+                        principalColumn: "tag_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_yeet_has_tags_yeets_yeet_id",
+                        column: x => x.yeet_id,
+                        principalTable: "yeets",
+                        principalColumn: "yeet_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "yomments",
+                columns: table => new
+                {
+                    yomment_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    yeet_id = table.Column<int>(type: "integer", nullable: false),
+                    body = table.Column<string>(type: "text", nullable: false),
+                    likes = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yomments", x => x.yomment_id);
+                    table.ForeignKey(
+                        name: "FK_yomments_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_yomments_yeets_yeet_id",
+                        column: x => x.yeet_id,
+                        principalTable: "yeets",
+                        principalColumn: "yeet_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_follows_users_jt_following_id",
+                table: "user_follows_users_jt",
+                column: "following_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_yeet_has_tags_tag_id",
+                table: "yeet_has_tags",
+                column: "tag_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_yeets_user_id",
+                table: "yeets",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_yomments_user_id",
+                table: "yomments",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_yomments_yeet_id",
+                table: "yomments",
+                column: "yeet_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_follows_users_jt");
+
+            migrationBuilder.DropTable(
+                name: "yeet_has_tags");
+
+            migrationBuilder.DropTable(
+                name: "yomments");
+
+            migrationBuilder.DropTable(
+                name: "tag");
+
+            migrationBuilder.DropTable(
+                name: "yeets");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
