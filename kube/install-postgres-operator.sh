9b41b5b212f14608138f377cd39fb65ea39ca8bb
Clemens Schlipfinger <clemens.schlipfinger@gmail.com>
2024-04-08 23:40:37 +0200

    kube

---
+++ kube/install-postgres-operator.sh
@@ -0,0 +1,5 @@
+# add repo for postgres-operator
+helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator
+
+# install the postgres-operator
+helm install postgres-operator postgres-operator-charts/postgres-operator
